15efeb8adef2cf427ccc37af1fc86afbc1ecebbf
Edith Estefany Pérez Trujillo <per26952@uvg.edu.gt>
2026-09-18 21:37:30 -0600

    Tutorial y su repositorio

---
+++ src/main/java/gt/edu/uvg/ecouvg/repositorio/RepositorioTutorial.java
@@ -0,0 +1,8 @@
+package gt.edu.uvg.ecouvg.repositorio;
+
+import gt.edu.uvg.ecouvg.modelo.Tutorial; 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioTutorial extends JpaRepository<Tutorial, Long> {
+
+}
